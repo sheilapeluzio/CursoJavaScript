32fbee6e3da9bee0e78176601d4d976f7b8359fa
Sheila Marilane Paula <you@example.com>
2021-07-24 15:26:03 -0300

    trabalhando com middlewares

---
+++ nodeJs/middlewares.js
@@ -0,0 +1,28 @@
+// middleware pattern (chain of responsability)
+
+const passo1 = (ctx, next) => {
+  ctx.valor1 = 'mid1'
+console.log(next)
+  next()
+
+
+}
+const passo2 = (ctx, next) => {
+  ctx.valor2 = 'mid2'
+  next()
+  
+}
+const passo3 = ctx => ctx.valor3 = 'mid3'
+  
+const exec = (ctx, ...middlewares) => {
+  const execPasso = indice => {
+    console.log(indice)
+    middlewares && indice < middlewares.length &&
+      middlewares[indice](ctx, () => execPasso(indice + 1))
+  
+  }
+execPasso(0)
+}
+const ctx = {}
+exec(ctx, passo1, passo2, passo3)
+console.log(ctx)
