da0ed728ad261e35c8f4db801767e7cd87b47fb4
sheil <sheil@DESKTOP-LA8J40P.local>
2021-07-24 22:37:16 -0300

    Projeto API com Express

---
+++ nodeJs/projeto/src/servidor.js
@@ -0,0 +1,11 @@
+const porta = 3003;
+
+const express = require("express");
+const app = express();
+
+app.get("/produtos", (req, res, next) => {
+  res.send({ nome: "Notebook", preco: 123.45 });
+});
+app.listen(porta, () => {
+  console.log(`Servidor esta execuntando na porta ${porta}`);
+});
